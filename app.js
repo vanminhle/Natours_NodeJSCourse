d28c1e02c490b48e60893b26cb081824843819db
vanminhle <vanminh.le.754@gmail.com>
2022-05-09 10:48:13 +0700

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+//get express app
 const app = express();
 
 //setting view engine
