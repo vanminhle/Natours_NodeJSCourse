b83bad7a6992194ae2d99ce0b6ac20cb63e7ee56
vanminhle <vanminh.le.754@gmail.com>
2022-05-11 16:59:34 +0700

    Final Course

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,38 +8,35 @@ exports.alerts = (req, res, next) => {
   const { alert } = req.query;
   if (alert === 'booking')
     res.locals.alert =
-      'Your booking is successfully, please check your email for confirmation. Please come back later if it not show up here!';
+      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
   next();
 };
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  //1) Get Tour Data from Collection
+  // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  //2) Build template
-
-  //3) Render that data template using tour data from 1
-
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours, //this is a array
+    tours,
   });
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  //1) Get the data, for the requested tour(including reviews and guides)
+  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    field: 'review rating user',
+    fields: 'review rating user',
   });
 
   if (!tour) {
-    return next(new AppError('There is no tour with that name', 404));
+    return next(new AppError('There is no tour with that name.', 404));
   }
-  //2) Build template
 
-  //3) Render template using data from 1
-
+  // 2) Build template
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour,
@@ -48,7 +45,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
-    title: 'Login into your account',
+    title: 'Log into your account',
   });
 };
 
@@ -59,22 +56,20 @@ exports.getAccount = (req, res) => {
 };
 
 exports.getMyTours = catchAsync(async (req, res, next) => {
-  //YOU CAN USING VIRTUAL POPULATE LIKE TOUR WITH REVIEWS HERE, IT WILL BE THE SAME
-  //1) find all booking of currently loggin user
+  // 1) Find all bookings
   const bookings = await Booking.find({ user: req.user.id });
 
-  //2) find tours with the returend ids
+  // 2) Find tours with the returned IDs
   const tourIDs = bookings.map((el) => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
   res.status(200).render('overview', {
-    title: 'My Booked Tours',
+    title: 'My Tours',
     tours,
   });
 });
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
-  //console.log('UPDATING USER', req.body);
   const updatedUser = await User.findByIdAndUpdate(
     req.user.id,
     {
