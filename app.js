781fa401fce9b1f543c7ef4df4a60cb3ae9e0d5c
vanminhle <vanminh.le.754@gmail.com>
2022-05-11 10:34:01 +0700

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 //get express app
@@ -109,6 +110,13 @@ const limiter = rateLimit({
   message: 'Too many request from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+//14-226 webhook checkout
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body parser, reading from data body into req.body
 app.use(express.json({ limit: '10kb' })); //limit 10kb json data body
