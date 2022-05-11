e49208c90ceb53947fe7a33434c6656803ef97c1
vanminhle <vanminh.le.754@gmail.com>
2022-05-11 16:56:11 +0700

    Final Course

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -9,7 +9,6 @@ export const hideAlert = () => {
 //type is 'success' or 'error'
 export const showAlert = (type, message, time = 7) => {
   hideAlert();
-
   const markup = `<div class="alert alert--${type}">${message}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-
-//console.log('Hello from Parcel');
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
@@ -8,49 +6,42 @@ import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
 import { showAlert } from './alerts';
 
-//DOM Elements
+// DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
-const logOutButton = document.querySelector('.nav__el--logout');
+const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 const bookBtn = document.getElementById('book-tour');
 
-//Delegations
+// DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
-  //console.log(locations);
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
 
-if (logOutButton) {
-  logOutButton.addEventListener('click', logout);
-}
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) {
+if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    //MULTIPART FORM DATA BECAUSE WE NOT ONLY HAVE STRING DATA BUT ALSO PHOTO OR FILE
     const form = new FormData();
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
-    //console.log(form);
 
     updateSettings(form, 'data');
   });
-}
 
-if (userPasswordForm) {
+if (userPasswordForm)
   userPasswordForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     document.querySelector('.btn--save-password').textContent = 'Updating...';
@@ -63,22 +54,18 @@ if (userPasswordForm) {
       'password'
     );
 
-    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.querySelector('.btn--save-password').textContent = 'Save password';
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
-}
 
-if (bookBtn) {
+if (bookBtn)
   bookBtn.addEventListener('click', (e) => {
     e.target.textContent = 'Processing...';
-    e.target.style.backgroundColor = '#000000';
-    //e.target -  target the click element button
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
-}
 
 const alertMessage = document.querySelector('body').dataset.alert;
 if (alertMessage) showAlert('success', alertMessage, 20);
